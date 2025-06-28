f18b8466341cac10861131b062f13f95182a1313
subbyte <klemen.remec@gmail.com>
2025-06-28 13:05:03 +0200

    cicd caching, robots fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,6 @@
 import { getBlogPosts } from './(pages)/blog/_lib/utils'
 
-export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
+export const baseUrl = 'https://subbyte.net'
 
 export default async function sitemap() {
   const posts = getBlogPosts()
