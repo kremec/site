7e4cc76a4d989aeb3ccffed3ec2842d1fb9728cd
subbyte <klemen.remec@gmail.com>
2025-06-28 13:13:13 +0200

    robots fix, cicd caching fix

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,10 +1,13 @@
 import { baseUrl } from './sitemap'
 
+export const dynamic = 'force-static'
+
 export default function robots() {
   return {
     rules: [
       {
         userAgent: '*',
+        allow: '/',
       },
     ],
     sitemap: `${baseUrl}/sitemap.xml`,
